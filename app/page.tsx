925ae4b160ef6c7cc8435eb1d466a53d5fcf3f5f
ur2910 <ushabarghavi.r@gmail.com>
2025-03-05 10:45:05 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a FinTech assistant ready to answer your questions!.
+              Hi, I'm a FinTech assistant ready to answer your questions!
             </h1>
             <p className="text-gray-500">
               I'm part of a series of computational experiments taught by{" "}
